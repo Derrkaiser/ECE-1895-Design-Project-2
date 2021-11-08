8c7ce7910cf41a9d7d2db0d7706b35f11ae27bc5
Matthew Kaiser <mpkaise@outlook.com>
2021-11-07 21:59:25 -0500

    Updated the OLED and SD/Audio test code

--- Arduino Code/OLED/OLED.ino
+++ Arduino Code/OLED/OLED.ino
@@ -4,24 +4,19 @@
 
 
 
-U8X8_SSD1306_128X64_NONAME_4W_SW_SPI OLED(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 7, /* dc=*/ 11, /* reset=*/ 4); /* Constructor for OLED display */
-
-//U8X8_SSD1306_128X64_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-// Need cs to be 10 because pin 10 of arduino pin 10 is the slave select/chip select pin, can change reset to 4 and dc to another if needed
+U8X8_SSD1306_128X64_NONAME_4W_SW_SPI OLED(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 7, /* dc=*/ 8, /* reset=*/ 4); /* Constructor for OLED display */
 
 
 void setup() {
   /* Setup necessary pins */
   OLED.begin();
-
-
+  OLED.setFont(u8x8_font_chroma48medium8_r);
 }
 
 void loop() {
   //put your main code here, to run repeatedly:
 
-  OLED.setFont(u8x8_font_chroma48medium8_r);
   OLED.drawString(0,0,"Hello World!");
-  delay(1000);
+  delay(5000);
 
 }
--- Arduino Code/SD-Card-Audio/SD-Card-Audio.ino
+++ Arduino Code/SD-Card-Audio/SD-Card-Audio.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "SD.h"
+#include "TMRpcm.h"
+#include "SPI.h"
 
+#define SD_ChipSelectPin 10
+
+
+TMRpcm tmrpcm;
+
+void setup(void) {
+ 
+  tmrpcm.speakerPin=9; // 9 for Uno, Nano, etc.
+
+  //Reads the chip
+  SD.begin(SD_ChipSelectPin);
+
+  //Sets the volume 
+  tmrpcm.setVolume(6);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(void) {
+  tmrpcm.play("test.wav");
+  delay(5000);
 }
